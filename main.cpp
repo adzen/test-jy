dcc19a2bfba5bba12033351e03071d53a9edbd9f
Ren-Yi Wong <101adzen@gmail.com>
2018-09-22 14:16:42 +0800

    Q1 (b)

--- main.cpp
+++ main.cpp
@@ -15,11 +15,35 @@ void f(char *input){
 	return;
 }
 
+void f2(char *input){
+	char ans[100] = {0};
+	int len = strlen(input);
+		
+	char *p = strtok(input, " ");
+	// char *q = p;
+	while(p != NULL){
+		// printf("%s\n", p);
+		f(p);
+		// printf("%s\n", p);
+		p = strtok(NULL, " ");
+		// q = p;
+	}
+
+	for(int i = 0; i < len; ++i){
+		if(input[i] == 0) input[i] = ' ';
+	}
+}
+
 int main(int argc, char *argv[]){
 	char t1[100] = "abcdefg";
 	printf("t1 = %s\n", t1);
 	f(t1);
 	printf("t1 = %s\n", t1);
+
+	char t2[100] = "asdfhjka asdjkhf fashjdfah fahsjfd";
+	printf("t2 = %s\n", t2);
+	f2(t2);
+	printf("t2 = %s\n", t2);
 
 	return 0;
 }
